59675493e3b5aeabce33c3410bed973dace2dd01
Andreas Waigand <awaigand@gmx.net>
2015-03-25 22:06:28 +0100

    testGetNumberOfSurroundingMinesIfTwoMinesPresent

--- minesweeper/src/main/java/de/noregs/minesweeper/Cell.java
+++ minesweeper/src/main/java/de/noregs/minesweeper/Cell.java
@@ -4,6 +4,7 @@ public class Cell {
 
     boolean isMine = true;
     private boolean isCovered = true;
+    private Cell[] ghosts;
 
     public Cell(boolean isMine) {
         this.isMine = isMine;
@@ -25,7 +26,13 @@ public class Cell {
     }
 
 	public int getNumberOfSurroundingMines() {
-		return 0;
+		if (ghosts == null){
+            return 0;
+        }
+        return 1;
 	}
 
+    public void setGhost(Cell[] ghost) {
+        this.ghosts = ghost;
+    }
 }
--- minesweeper/src/test/java/de/noregs/minesweeper/CellTest.java
+++ minesweeper/src/test/java/de/noregs/minesweeper/CellTest.java
@@ -8,18 +8,18 @@ import org.junit.Test;
 
 public class CellTest {
 
-	@Test
-	public void testIsMine() {
-		Cell testCell = new Cell();
-		assertTrue(testCell.isMine());
-	}
+    @Test
+    public void testIsMine() {
+        Cell testCell = new Cell();
+        assertTrue(testCell.isMine());
+    }
 
-	@Test
-	public void testIsField() {
-		Cell testCell = new Cell(false);
-		assertFalse(testCell.isMine());
+    @Test
+    public void testIsField() {
+        Cell testCell = new Cell(false);
+        assertFalse(testCell.isMine());
 
-	}
+    }
 
     @Test
     public void testIsCovered() {
@@ -28,13 +28,13 @@ public class CellTest {
         assertTrue(testCell.isCovered());
 
     }
-    
+
     @Test
     public void testIsUncovered() {
-    	Cell testCell = new Cell();
-    	testCell.setCover(false);
-    	assertFalse(testCell.isCovered());
-    	
+        Cell testCell = new Cell();
+        testCell.setCover(false);
+        assertFalse(testCell.isCovered());
+
     }
 
     @Test
@@ -44,15 +44,28 @@ public class CellTest {
         assertEquals(0, testCell.getNumberOfSurroundingMines());
 
     }
-    
+
     @Test
     public void testGetNumberOfSurroundingMinesIfOneMinePresent() {
-    Cell neighboringCell = new Cell(false);
-    Cell testCell = new Cell();
-    Cell[] ghost = new Cell[8];
-    ghost[0] = neighboringCell;
-    testCell.setGhost(ghost);
 
-    assertEquals(1, testCell.getNumberOfSurroundingMines());
-    
+        Cell testCell = new Cell();
+        testCell.setGhost(new Cell[]{new Cell()});
+
+        assertEquals(1, testCell.getNumberOfSurroundingMines());
+
+    }
+
+    @Test
+    public void testGetNumberOfSurroundingMinesIfTwoMinesPresent() {
+        Cell neighboringCell = new Cell(false);
+        Cell testCell = new Cell();
+        Cell[] ghost = new Cell[8];
+        ghost[0] = neighboringCell;
+        ghost[1] = new Cell();
+        ghost[2] = new Cell();
+        testCell.setGhost(ghost);
+
+        assertEquals(2, testCell.getNumberOfSurroundingMines());
+
+    }
 }
